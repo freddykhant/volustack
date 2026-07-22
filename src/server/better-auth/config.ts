21456127f83e8e6bdf71a699e8426b4a15d038e1
Aaron <aaron.tan1@gmail.com>
2026-07-22 11:14:43 +0800

    feat(oauth): add prompt option for Google social provider

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -13,6 +13,7 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
+      prompt: "select_account",
       clientId: env.BETTER_AUTH_GOOGLE_CLIENT_ID,
       clientSecret: env.BETTER_AUTH_GOOGLE_CLIENT_SECRET,
       // redirectURI: "http://localhost:3000/api/auth/callback/google",
